f05e824600b659a00224b91e6f480abeea485b87
reyhansaja <raihan.ts16b@gmail.com>
2024-09-04 16:02:24 +0700

    raihan nambah main

--- perkenalan.java
+++ perkenalan.java
@@ -6,5 +6,9 @@ public class perkenalan {
     public static void main(String[] args) {
         System.out.println("Hai kami dari kelompok CIHUYY");
         System.out.println("Anggota kelompok kami adalah:");
+        System.out.println("Nama\t: Raihan Nur Pratama");
+        System.out.println("Kelas\t: TI 1A");
+        System.out.println("NIM\t: 244107020085");
+        System.out.println("Tanggal lahir: 13-07-2006");
     }
 }
