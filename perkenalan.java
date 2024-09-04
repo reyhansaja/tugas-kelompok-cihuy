02c6c149e4bfb5d24cb6f74f5f71e625f6394cd9
reyhansaja <raihan.ts16b@gmail.com>
2024-09-04 15:56:29 +0700

    ganti

--- perkenalan.java
+++ perkenalan.java
@@ -6,17 +6,5 @@ public class perkenalan {
     public static void main(String[] args) {
         System.out.println("Hai kami dari kelompok CIHUYY");
         System.out.println("Anggota kelompok kami adalah:");
-        System.out.println("\t 1. Nama\t:");
-        System.out.println("\t    Kelas\t:");
-        System.out.println("\t    NIM\t:");
-        System.out.println("\t    Prodi\t:");
-        System.out.println("\t 2. Nama\t:");
-        System.out.println("\t    Kelas\t:");
-        System.out.println("\t    NIM\t:");
-        System.out.println("\t    Prodi\t:");
-        System.out.println("\t 3. Nama\t:");
-        System.out.println("\t    Kelas\t:");
-        System.out.println("\t    NIM\t:");
-        System.out.println("\t    Prodi\t:");
     }
 }
