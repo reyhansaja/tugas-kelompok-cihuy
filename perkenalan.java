89f150c50d69d3b39eccd5666234d377b94fe6b5
rafa-jel <rafaradana2@gmail.com>
2024-09-04 16:07:35 +0700

    anjay

--- perkenalan.java
+++ perkenalan.java
@@ -6,5 +6,9 @@ public class perkenalan {
     public static void main(String[] args) {
         System.out.println("Hai kami dari kelompok CIHUYY");
         System.out.println("Anggota kelompok kami adalah:");
+        System.out.println("Nama\t: Rafazl Radana Diva");
+        System.out.println("Kelas\t: 1A");
+        System.out.println("NIM\t: 244107020082");
+        System.out.println("Tanggal Lahir\t: 15 November 2005");
     }
 }
