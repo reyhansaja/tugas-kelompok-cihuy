4154615fd30b42e79b933963bac767f9fd24aece
RayaRabbani129 <arya.rabbani06@gmail.com>
2024-09-04 16:06:34 +0700

    Aku mirip JI CHANG WOOK

--- perkenalan.java
+++ perkenalan.java
@@ -6,5 +6,9 @@ public class perkenalan {
     public static void main(String[] args) {
         System.out.println("Hai kami dari kelompok CIHUYY");
         System.out.println("Anggota kelompok kami adalah:");
+        System.out.println("Nama\t:Raya Rabbani Ananta A");
+        System.out.println("Kelas\t: TI 1A");
+        System.out.println("NIM\t: 244107020159");
+        System.out.println("Tanggal Lahir\t: 12-01-2006");
     }
 }
